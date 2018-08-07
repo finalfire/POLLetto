7ebf5d43669049e3c8ce1fe0cb39054510bb0bb0
finalfire <f.cauteruccio@gmail.com>
2018-08-07 16:28:15 +0200

    removed token

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from poll import *
 from telegram.ext import CommandHandler, Updater
 import logging
 
+def parse_params(msg):
+    params = msg.split(' ')[1:]
+    logging.info('Num of pool params: {}'.format(len(params)))
+    for i, opt in enumerate(params):
+        logging.info('Param {}: {}'.format(i, opt))
+    return params
+
 def help_message():
     return '''Weee, io sono un  POLLetto e ti aiuto a fare roba strana quale creare POLL, (ovvero sondaggi).
 Questo è un messaggio di help che hai invocato utilizzando il comando /help. Mo tu tiani.'''
@@ -15,12 +22,39 @@ def help(bot, update):
     """Handles the /help command by showing all of the commands."""
     bot.send_message(chat_id=update.message.chat_id, text=help_message())
 
+def poll_handler(bot, update):
+    """Handles all polls commands."""
+    params = parse_params(update.message.text)
+
+    logging.info('/param command')
+
+    # single params: list
+    if len(params) == 1:
+        if params[0] == 'list':
+            # ...
+            logging.info('List of available polls is requested!')
+    elif len(params) > 1:
+        if params[0] == 'create':
+            # ...
+            logging.info('Creation of a new poll is requested!')
+            return
+        
+        if params[0] == 'delete':
+            # ...
+            logging.info('Deletion of a new poll is requested!')
+            return
+
+        if params[0] == 'view':
+            # ...
+            logging.info('View of a poll is requested!')
+            return
+
 def start_bot():
     # logging utility
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
     # the update
-    updater = Updater(token='270200425:AAFJpImg5vlnG7xoWmBelIYhpVYm_sQ77-Y')
+    updater = Updater(token='')
     disp = updater.dispatcher
 
     # commands handlers
